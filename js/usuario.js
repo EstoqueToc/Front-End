1b06b17f69ea9d61429f4e5db63eae72f3203c86
jhonathaGC <gabrielljjhonatha@gmail.com>
2024-04-03 19:59:17 -0300

    @jhonathaGC atualizando trasição de NavBar

---
+++ js/usuario.js
@@ -0,0 +1,8 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "20%";
+    document.getElementsByClassName("tabela-usuario").style.width="80%";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
