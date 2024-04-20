97fe404d5215d83f76c2c332daf6f0e3d87ad0c2
Ciliberti <arthur.coliveira@sptech.school>
2024-04-20 13:12:05 -0300

    recover

---
+++ js/LoginCadastro.js
@@ -0,0 +1,10 @@
+const container = document.getElementById('container')
+const cadastrarBtn = document.getElementById('cadastrar-se')
+const entrarBtn = document.getElementById('entrar')
+
+cadastrarBtn.addEventListener('click', () => {
+    container.classList.add("active");
+});
+entrarBtn.addEventListener('click', () => {
+    container.classList.remove("active");
+});
