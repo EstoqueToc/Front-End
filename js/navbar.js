d0d519956d43c1c18a0f9919a78d79679feca824
Isabel Fagundes <isabel.fagundes@sptech.school>
2024-04-23 17:05:11 -0300

    @isabelCalberti Subindo tela Cadastro Funcionario

--- js/navbar.js
+++ js/navbar.js
@@ -1,14 +1,14 @@
 function toggleSubmenu(id) {
-    var submenu = document.getElementById(id);
-    var icon = submenu.previousElementSibling.querySelector('i.fa-angle-down');
+  var submenu = document.getElementById(id);
+  var icon = submenu.previousElementSibling.querySelector('i.fa-angle-down');
 
-    if (submenu.style.display === 'none') {
-      submenu.style.display = 'block';
-      icon.classList.remove('fa-angle-down');
-      icon.classList.add('fa-angle-up');
-    } else {
-      submenu.style.display = 'none';
-      icon.classList.remove('fa-angle-up');
-      icon.classList.add('fa-angle-down');
-    }
+  if (submenu.style.display === 'none') {
+    submenu.style.display = 'block';
+    icon.classList.remove('fa-angle-down');
+    icon.classList.add('fa-angle-up');
+  } else {
+    submenu.style.display = 'none';
+    icon.classList.remove('fa-angle-up');
+    icon.classList.add('fa-angle-down');
   }
+}
